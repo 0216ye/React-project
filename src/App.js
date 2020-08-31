f6278367aa063b59c631f094c50a0d8b78beaf28
yongjie <2246106715@qq.com>
2020-08-31 22:24:29 +0800

    redux的学习

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, {Component} from 'react'
-import {Route,Switch} from 'react-router-dom'
-import Admin from './pages/Admin/Admin'
-import Login from './pages/Login/Login'
-import './App.less'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route path = "/login" component={Login} />
-          <Route path = "/admin" component={Admin} />
-        </Switch>
+        App
       </div>
     )
   }
