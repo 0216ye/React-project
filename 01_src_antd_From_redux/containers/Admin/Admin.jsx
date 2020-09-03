dc65632d018d3ed0055a2e0e0a34441ad5949767
yongjie <2246106715@qq.com>
2020-09-03 10:45:30 +0800

    表单+redux+antd

---
+++ 01_src_antd_From_redux/containers/Admin/Admin.jsx
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+import {connect} from 'react-redux'
+import {createDemo1Action} from '../../redux/action_creators/test_action'
+class Admin  extends Component{
+    componentDidMount(){
+        console.log(this.props)
+    }
+
+    render(){
+       return (
+           <div>    
+               Admin
+           </div>
+       )
+   }
+}
+export default connect(
+    state => ({peiqi:state.test}),
+    {
+        demo1 :createDemo1Action
+    }
+)(Admin)
---
+++ 01_src_antd_From_redux/redux/action_types.js
@@ -0,0 +1,4 @@
+//定义type常量的文件 
+
+export const TEST1 = 'test1'
+export const TEST2 = 'test2'
